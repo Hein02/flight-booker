8cf7a6ab7c4b69a1735ac8a3778bad6ee052505d
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-15 08:20:18 -0800

    Create an Airport model
    Add several airports, using the db/seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.delete_all
+
+Airport.create(
+  [
+    {
+      code: 'SFO'
+    },
+    {
+      code: 'NYC'
+    }
+  ]
+)
