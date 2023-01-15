011b2d5f709ddab78e08f30fb74d76a769978041
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-15 09:31:50 -0800

    Add index route for FlightsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'flights#index'
+
+  resources :flights, only: %i[index]
 end
