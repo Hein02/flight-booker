25a450ddedc6ed4e94fdc6cad479a7d593a3277e
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-15 09:05:01 -0800

    Seed db with more airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,58 @@ Airport.create(
     {
       code: 'NYC',
       id: 2
+    },
+    {
+      code: 'ATL',
+      id: 3
+    },
+    {
+      code: 'LAX',
+      id: 4
+    },
+    {
+      code: 'ORD',
+      id: 5
+    },
+    {
+      code: 'DFW',
+      id: 6
+    },
+    {
+      code: 'DEN',
+      id: 7
+    },
+    {
+      code: 'JFK',
+      id: 8
+    },
+    {
+      code: 'SEA',
+      id: 9
+    },
+    {
+      code: 'MCO',
+      id: 10
+    },
+    {
+      code: 'LAS',
+      id: 11
+    },
+    {
+      code: 'CLT',
+      id: 12
+    },
+    {
+      code: 'EWR',
+      id: 13
+    },
+    {
+      code: 'PHX',
+      id: 14
+    },
+    {
+      code: 'IAH',
+      id: 15
     }
   ]
 )
@@ -32,6 +84,38 @@ Flight.create(
       departure_airport_id: 1,
       arrival_airport_id: 2,
       id: 1
+    },
+    {
+      departure_time: Time.now + (3 * 7 * 24 * 3600),
+      arrival_time: Time.now + (3 * 7 * 26 * 3600),
+      price: 320.50,
+      departure_airport_id: 2,
+      arrival_airport_id: 4,
+      id: 2
+    },
+    {
+      departure_time: Time.now + (2 * 8 * 24 * 3600),
+      arrival_time: Time.now + (2 * 8 * 26 * 3600),
+      price: 270.00,
+      departure_airport_id: 10,
+      arrival_airport_id: 12,
+      id: 3
+    },
+    {
+      departure_time: Time.now + (2 * 10 * 24 * 3600),
+      arrival_time: Time.now + (2 * 10 * 30 * 3600),
+      price: 130.00,
+      departure_airport_id: 15,
+      arrival_airport_id: 2,
+      id: 4
+    },
+    {
+      departure_time: Time.now + (2 * 12 * 24 * 3600),
+      arrival_time: Time.now + (2 * 12 * 32 * 3600),
+      price: 235.00,
+      departure_airport_id: 6,
+      arrival_airport_id: 9,
+      id: 5
     }
   ]
 )
