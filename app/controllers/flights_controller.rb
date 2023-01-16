3d0bb582ee853ec949ceee641c7f625b172c9bb4
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-16 09:56:17 -0800

    Refactor FlightsController#index
    Add #search_flights for querying flights.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,22 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @available_flights = Flight.all
-
     @date = params[:date] || Date.today.to_s
+
+    @flights = search_flights
+    @num_passengers = params[:num_passengers]
+  end
+
+  private
+
+  def search_flights
     start_time = Date.parse(@date).beginning_of_day
     end_time = Date.parse(@date).end_of_day
-    @flights = Flight.where(
+    Flight.where(
       departure_airport_id: params[:departure],
       arrival_airport_id: params[:arrival],
       # https://stackoverflow.com/questions/13211916/rails-activerecord-find-search-by-date
       departure_time: start_time..end_time
     )
-    @num_passengers = params[:num_passengers]
   end
 end
