7e78bace76a6f7309710999f9ccaeb31af218853
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-16 13:01:38 -0800

    Build form for Booking#create action
    Make booking flight works.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,25 @@ class BookingsController < ApplicationController
     flight_id = params[:flight]
     @flight = Flight.find(flight_id)
   end
+
+  def create
+    flight = Flight.find(booking_params[:flight_id])
+    passengers = booking_params[:passengers]
+    booking = flight.bookings.build
+    booking.passengers.build(passengers)
+    if booking.save
+      flash[:success] = 'Congratulation! Enjoy your trip.'
+      redirect_to flights_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    # To permit nested parameters in Strong parameters
+    # https://github.com/rails/strong_parameters#nested-parameters
+    params.require(:booking).permit(:flight_id, passengers: %i[name email])
+  end
 end
