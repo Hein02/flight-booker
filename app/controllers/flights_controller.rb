d78df3ff4b6767b77ab9a8460e7657fcd8e08452
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-16 08:28:18 -0800

    Build search form for flights
    Update FlightsController#index to show the search results.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,16 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+    @available_flights = Flight.all
+
+    @date = params[:date] || Date.today.to_s
+    start_time = Date.parse(@date).beginning_of_day
+    end_time = Date.parse(@date).end_of_day
+    @flights = Flight.where(
+      departure_airport_id: params[:departure],
+      arrival_airport_id: params[:arrival],
+      # https://stackoverflow.com/questions/13211916/rails-activerecord-find-search-by-date
+      departure_time: start_time..end_time
+    )
   end
 end
