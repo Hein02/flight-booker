564de2553180c4aca22790d8c22f6f4a556c17bc
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-16 11:12:14 -0800

    Create and migrate Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_172427) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_190920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,10 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_172427) do
     t.string "tz"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "flights", force: :cascade do |t|
     t.datetime "departure_time"
     t.datetime "arrival_time"
-    t.decimal "price"
+    t.decimal "price", precision: 10, scale: 2
     t.bigint "arrival_airport_id", null: false
     t.bigint "departure_airport_id", null: false
     t.datetime "created_at", null: false
